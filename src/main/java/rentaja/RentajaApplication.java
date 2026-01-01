c1ae31e35055e7a6b8e9b9177faca307dcaa4c73
Haris Nurryanto <harisnurryanto11625@gmail.com>
2026-01-01 16:27:07 +0700

    first commit

---
+++ src/main/java/rentaja/RentajaApplication.java
@@ -0,0 +1,13 @@
+package rentaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rentaja/RentajaApplicationTests.java
@@ -0,0 +1,13 @@
+package rentaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
